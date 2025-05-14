cc538d5bed6f607025d23fada5d6fe5225b8944f
shala19 <sara1990pulido@gmail.com>
2025-05-13 22:09:59 -0500

    Agrego PHPMailer con Composer correctamente

--- Model/UserModel.php
+++ Model/UserModel.php
@@ -16,7 +16,7 @@ class UserModel {
         return $stmt->get_result()->fetch_assoc();
     }
     public function updatePassword($email, $newPassword) {
-        $stmt = $this->db->prepare("UPDATE usuarios SET contrasena = ? WHERE email_usuario = ?");
+        $stmt = $this->db->prepare("UPDATE usuarios SET contrasena = ? WHERE correo = ?");
         $stmt->bind_param("ss", $newPassword, $email);
         
         return $stmt->execute();
